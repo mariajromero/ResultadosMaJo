284cdf5fc02239bca3ff4aa75dd4e59776edd0d7
mariajromero <mariajromerov@outlook.com>
2022-12-05 15:23:04 -0500

    cambia el nombre de carpetas

---
+++ mariaRomero/src/app/shared/postre.ts
@@ -0,0 +1,7 @@
+export class Postre{
+    name:string;
+    id:String;
+    image:string;
+    category:string;
+    description:string;
+}
---
+++ mariaRomero/src/app/ver-mas/ver-mas.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-ver-mas',
+  templateUrl: './ver-mas.component.html',
+  styleUrls: ['./ver-mas.component.scss']
+})
+export class VerMasComponent implements OnInit {
+  
+  
+  constructor() { }
+
+  ngOnInit()  {
+ 
+  }
+  
+
+}
