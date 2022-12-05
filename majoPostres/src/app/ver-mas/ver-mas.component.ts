cbf7ae6f3f6e95e38ef6ef25c413f1e0085822ea
mariajromero <mariajromerov@outlook.com>
2022-12-05 14:56:02 -0500

    habilidades blandas

--- majoPostres/src/app/ver-mas/ver-mas.component.ts
+++ majoPostres/src/app/ver-mas/ver-mas.component.ts
@@ -1,31 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import {Postre} from '../shared/postre';
-import { PostreService } from '../shared/postre.service';
-import { HttpErrorResponse } from '@angular/common/http';
+
 @Component({
   selector: 'app-ver-mas',
   templateUrl: './ver-mas.component.html',
   styleUrls: ['./ver-mas.component.scss']
 })
 export class VerMasComponent implements OnInit {
-  public postres:Postre[];
   
-  constructor(private postreService: PostreService) { }
+  
+  constructor() { }
 
   ngOnInit()  {
-    this.getPostres();
+ 
   }
-  public getPostres():void{
-    this.postreService.getPostres().subscribe(
-      (response:Postre[])=>{
-        this.postres=response;
-      },
-      (error:HttpErrorResponse)=>{
-        alert(error.message);
-      }
-      
-    );
-  }
+  
 
 }
